3a73004f0afb5eae08cd41b594f1a6d50d238401
Eduarda Ferreira Lima <137132951+flduda@users.noreply.github.com>
2024-05-28 10:42:02 -0700

    button change color

--- src/primeiroProjeto/app.js
+++ src/primeiroProjeto/app.js
@@ -6,4 +6,11 @@ function sumOne (){
     let value = parseInt(elementoH3.textContent)
     let newValue = value + 1 
     elementoH3.textContent = newValue;
-};
+
+    //if the value is =>5
+
+if (newValue == 5) {
+    elementoH3.style.color = 'red'
+}
+}
+
